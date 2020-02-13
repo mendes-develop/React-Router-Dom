91f8148a1bb168f0f88cf8db4f84a2a35ff1e6d1
Alex Mendes <mendes.develop@gmail.com>
2020-02-13 03:03:44 -0600

    adding app.css

--- src/App.js
+++ src/App.js
@@ -6,12 +6,6 @@ import Contact from './components/Contact';
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-
-
-
-
-
-
 import './App.css'
 
 class App extends React.Component {
